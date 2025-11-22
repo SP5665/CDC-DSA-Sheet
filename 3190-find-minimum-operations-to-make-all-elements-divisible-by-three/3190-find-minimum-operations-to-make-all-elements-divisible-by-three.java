8ef39e72db9e194fb7207a25fb881e1d9414b832
Srishti Pandey <srishtipandey0512@gmail.com>
2025-11-23 01:17:55 +0530

    Delete 3190-find-minimum-operations-to-make-all-elements-divisible-by-three directory

--- 3190-find-minimum-operations-to-make-all-elements-divisible-by-three/3190-find-minimum-operations-to-make-all-elements-divisible-by-three.java
+++
@@ -1,9 +0,0 @@
-class Solution {
-    public int minimumOperations(int[] nums) {
-        int c=0;
-        for (int num : nums) {
-            if (num%3 != 0) c++;
-        }
-        return c;
-    }
-}
