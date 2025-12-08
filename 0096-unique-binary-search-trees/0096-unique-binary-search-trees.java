60132b236cdb1a6215678ac1fcb4dce1acfe0e1c
Srishti Pandey <srishtipandey0512@gmail.com>
2025-12-08 22:34:18 +0530

    Time: 0 ms (100%), Space: 40.4 MB (83.13%) - LeetHub

---
+++ 0096-unique-binary-search-trees/0096-unique-binary-search-trees.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int numTrees(int n) {
+        long[] dp = new long[n + 1];
+        dp[0] = dp[1] = 1;
+        for (int nodes = 2; nodes <= n; nodes++) {
+            for (int root = 1; root <= nodes; root++) {
+                dp[nodes] += dp[root - 1] * dp[nodes - root];
+            }
+        }
+        return (int) dp[n];
+    }
+}
