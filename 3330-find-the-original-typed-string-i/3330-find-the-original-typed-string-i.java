c285ab8454e4e486f4f9de07bc9bda8bc3217d91
Srishti Pandey <srishtipandey0512@gmail.com>
2025-12-10 21:04:27 +0530

    Delete 3330-find-the-original-typed-string-i directory

--- 3330-find-the-original-typed-string-i/3330-find-the-original-typed-string-i.java
+++
@@ -1,9 +0,0 @@
-class Solution {
-    public int possibleStringCount(String word) {
-        int c=1;
-        for (int i=0; i<word.length()-1; i++) {
-            if (word.charAt(i) == word.charAt(i+1)) c++;
-        }
-        return c;
-    }
-}
