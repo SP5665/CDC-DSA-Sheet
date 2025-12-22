92f0f3d3523af7493133e396a922e139b155ffbf
Srishti Pandey <srishtipandey0512@gmail.com>
2025-12-22 16:48:58 +0530

    Time: 103 ms (43.2%), Space: 51.3 MB (28.09%) - LeetHub

---
+++ 0204-count-primes/0204-count-primes.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int countPrimes(int n) {
+        if(n <= 2) return 0;
+        boolean ans[] = new boolean[n];
+        int c = 0;
+        ans[0] = ans[1] = false;
+        for (int i=2; i<n; i++) ans[i] = true;
+        for (int i=2; i<Math.sqrt(n); i++) {
+            if (ans[i]) {
+                for (int j=i*i; j<n; j+=i) ans[j] = false;
+            }
+        }
+        for (int i=2; i<n; i++) {
+            if(ans[i]) c++;
+        }
+        return c;
+    }
+}
