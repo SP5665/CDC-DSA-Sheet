042952e1d2f260548cfd01dfb23e49cfb803d179
Srishti Pandey <srishtipandey0512@gmail.com>
2025-12-26 12:56:56 +0530

    Time: 42 ms (72.64%), Space: 108.9 MB (86.57%) - LeetHub

---
+++ 3075-maximize-happiness-of-selected-children/3075-maximize-happiness-of-selected-children.java
@@ -0,0 +1,15 @@
+class Solution {
+    public long maximumHappinessSum(int[] happiness, int k) {
+        Arrays.sort(happiness);
+        int i = happiness.length-1;
+        long res = 0; int val = 0;
+        while (k>0) {
+            long a = happiness[i--]-val;
+            if (a<=0) break;
+            res += a;
+            val++;
+            k--;
+        }
+        return res;
+    }
+}
