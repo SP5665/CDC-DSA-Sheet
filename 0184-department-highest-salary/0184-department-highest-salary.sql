1f8ca7890a9534a652399d4e24ab7e71d43381b4
Srishti Pandey <srishtipandey0512@gmail.com>
2025-12-27 23:30:34 +0530

    Time: 945 ms (24.59%), Space: 0B (100%) - LeetHub

--- 0184-department-highest-salary/0184-department-highest-salary.sql
+++ 0184-department-highest-salary/0184-department-highest-salary.sql
@@ -1,5 +1,7 @@
 # Write your MySQL query statement below
-SELECT d.name AS Department, e.name AS Employee, e.salary AS Salary
+SELECT d.name AS Department,
+       e.name AS Employee,
+       e.salary AS Salary
 FROM Employee AS e
 JOIN Department AS d
 ON e.departmentId = d.id
