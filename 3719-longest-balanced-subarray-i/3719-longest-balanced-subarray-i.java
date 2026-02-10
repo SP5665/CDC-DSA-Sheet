253b601b6358a3c949b444be1649e0230d63dda9
Srishti Pandey <srishtipandey0512@gmail.com>
2026-02-10 22:01:33 +0530

    Time: 382 ms (22.99%), Space: 47 MB (68.61%) - LeetHub

--- 3719-longest-balanced-subarray-i/3719-longest-balanced-subarray-i.java
+++ 3719-longest-balanced-subarray-i/3719-longest-balanced-subarray-i.java
@@ -2,16 +2,16 @@ class Solution {
     public int longestBalanced(int[] nums) {
         int n = nums.length;
         int maxLen = 0;
-        for (int i=0; i<n; i++) { //decides where subarray starts
+        for (int i=0; i<n; i++) {
             Set<Integer> even = new HashSet<>();
             Set<Integer> odd = new HashSet<>();
-            for (int j=i; j<n; j++) { //decides where subarray ends
+            for (int j=i; j<n; j++) {
                 if (nums[j] % 2 == 0) {
                     even.add(nums[j]);
                 } else {
                     odd.add(nums[j]);
                 }
-                if (even.size() == odd.size()) {
+                if (even.size()==odd.size()) {
                     maxLen = Math.max(maxLen, j-i+1);
                 }
             }
