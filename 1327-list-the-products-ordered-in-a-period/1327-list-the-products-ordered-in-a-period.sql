d0697e45abf47a244543e51e44d7626296350da0
Srishti Pandey <srishtipandey0512@gmail.com>
2026-04-03 23:56:32 +0530

    Time: 704 ms (50.05%), Space: 0B (100%) - LeetHub

--- 1327-list-the-products-ordered-in-a-period/1327-list-the-products-ordered-in-a-period.sql
+++ 1327-list-the-products-ordered-in-a-period/1327-list-the-products-ordered-in-a-period.sql
@@ -4,5 +4,5 @@ FROM Products AS p
 JOIN Orders AS o
 ON p.product_id = o.product_id
 WHERE o.order_date BETWEEN '2020-02-01' AND '2020-02-29'
-GROUP BY product_name
+GROUP BY p.product_id
 HAVING SUM(o.unit) >= 100;
