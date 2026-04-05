0d79efbfca7b76cadd5c2c7e0bae9c23a5e930ea
Srishti Pandey <srishtipandey0512@gmail.com>
2026-04-05 21:34:56 +0530

    Time: 720 ms (59.41%), Space: 0B (100%) - LeetHub

--- 1517-find-users-with-valid-e-mails/1517-find-users-with-valid-e-mails.sql
+++ 1517-find-users-with-valid-e-mails/1517-find-users-with-valid-e-mails.sql
@@ -1,15 +1,11 @@
 # Write your MySQL query statement below
 SELECT * FROM Users
-WHERE 
-    mail REGEXP '^[a-zA-Z][a-zA-Z0-9_.-]*@leetcode\\.com$' 
-    AND mail LIKE BINARY '%@leetcode.com';
+WHERE REGEXP_LIKE(mail, '^[A-Za-z][a-zA-Z0-9_.-]*@leetcode[.]com$', 'c');
 
--- REGEXP:
--- ^ → start of string
--- [a-zA-Z] → first character must be a letter
--- [a-zA-Z0-9_.-]* → allowed characters (letters, digits, _, ., -)
--- @leetcode\.com → must end with this domain
--- $ → end of string
-
--- LIKE BINARY:
--- ensures email ends with lowecase '@leetcode.com'
+-- REGEXP_LIKE:
+-- ^ -> start of string
+-- [a-zA-Z] -> first character must be a letter
+-- [a-zA-Z0-9_.-]* -> allowed characters (letters, digits, _, ., -)
+-- @leetcode\.com -> must end with this domain
+-- $ -> end of string
+-- c -> case sensitive, i is for insensitive
