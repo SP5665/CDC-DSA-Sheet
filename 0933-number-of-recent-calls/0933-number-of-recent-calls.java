9813077e5f1020eb8fb48abb62cf85a5be4e22da
Srishti Pandey <srishtipandey0512@gmail.com>
2026-07-11 01:06:17 +0530

    Time: 19 ms (95.88%), Space: 62.2 MB (5.04%) - LeetHub

---
+++ 0933-number-of-recent-calls/0933-number-of-recent-calls.java
@@ -0,0 +1,20 @@
+class RecentCounter {
+    ArrayList<Integer> list = new ArrayList<>();
+    int start;
+    public RecentCounter() {
+        start = 0;
+    }
+    public int ping(int t) {
+        while (start < list.size() && (t - list.get(start) > 3000)) { 
+            start++;
+        }
+        list.add(t);
+        return list.size() - start;
+    }
+}
+
+/**
+ * Your RecentCounter object will be instantiated and called as such:
+ * RecentCounter obj = new RecentCounter();
+ * int param_1 = obj.ping(t);
+ */
