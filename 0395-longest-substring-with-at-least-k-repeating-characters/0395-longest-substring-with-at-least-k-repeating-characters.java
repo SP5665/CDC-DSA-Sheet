f0037ea5eb58623f5da048f78d26b5977ce90e87
Srishti Pandey <srishtipandey0512@gmail.com>
2026-07-15 22:36:46 +0530

    Time: 67 ms (24.54%), Space: 47.4 MB (12.89%) - LeetHub

--- 0395-longest-substring-with-at-least-k-repeating-characters/0395-longest-substring-with-at-least-k-repeating-characters.java
+++ 0395-longest-substring-with-at-least-k-repeating-characters/0395-longest-substring-with-at-least-k-repeating-characters.java
@@ -1,34 +1,19 @@
 class Solution {
     public int longestSubstring(String s, int k) {
-        int ans = 0;
         int n = s.length();
-        for (int i=1; i<=26; i++) {
-            HashMap<Character, Integer> hm = new HashMap<>();
-            int l = 0;
-            int r = 0;
-            int unique = 0;
-            int atLeastK = 0;
-            while (r < n) {
-                char c = s.charAt(r);
-                hm.put(c, hm.getOrDefault(c, 0)+1);
-                if (hm.get(c) == 1) unique++;
-                if (hm.get(c) == k) atLeastK++;
-                r++;
-                while (unique > i) {
-                    char ch = s.charAt(l);
-                    if (hm.get(ch) == k) atLeastK--;
-                    hm.put(ch, hm.get(ch) - 1);
-                    if (hm.get(ch) == 0) {
-                        hm.remove(ch);
-                        unique--;
-                    }
-                    l++;
-                }
-                if (unique == i && unique == atLeastK) {
-                    ans = Math.max(ans, r-l);
-                }
+        int[] freq = new int[26];
+        for (char ch : s.toCharArray()) {
+            freq[ch - 'a']++;
+        }
+        for (int i=0; i<n; i++) {
+            if (freq[s.charAt(i) - 'a'] < k) { //bad character
+                //left of bad character
+                int left = longestSubstring(s.substring(0, i), k);
+                //right of bad character
+                int right = longestSubstring(s.substring(i + 1), k);
+                return Math.max(left, right);
             }
         }
-        return ans;
+        return n;
     }
 }
