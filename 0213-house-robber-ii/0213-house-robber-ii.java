c86db81e961a21784791cf918e22f5e3d407c21a
Srishti Pandey <srishtipandey0512@gmail.com>
2026-09-13 18:39:22 +0530

    Time: 0 ms (100%), Space: 42.8 MB (60.08%) - LeetHub

--- 0213-house-robber-ii/0213-house-robber-ii.java
+++ 0213-house-robber-ii/0213-house-robber-ii.java
@@ -3,10 +3,15 @@ class Solution {
         int n = nums.length;
         if (n == 1) return nums[0];
         if (n == 2) return Math.max(nums[0], nums[1]);
-        int[] dp1 = Arrays.copyOfRange(nums, 0, n - 1);
-        int[] dp2 = Arrays.copyOfRange(nums, 1, n);
-        dp1[1] = Math.max(dp1[0], dp1[1]);
-        dp2[1] = Math.max(dp2[0], dp2[1]);
+        
+        int[] dp1 = new int[n-1];
+        int[] dp2 =new int[n-1];
+
+        dp1[0] = nums[0];
+        dp1[1] = Math.max(nums[0], nums[1]);
+        dp2[0] = nums[1];
+        dp2[1] = Math.max(nums[1], nums[2]);
+
         for (int i = 2; i < dp1.length; i++) {
             dp1[i] = Math.max(dp1[i-1], dp1[i-2] + nums[i]);
         }
