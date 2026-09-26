41348a4d03e67af7dc99b972313a19246313d02c
Srishti Pandey <srishtipandey0512@gmail.com>
2026-09-26 19:20:44 +0530

    Time: 31 ms (96.83%), Space: 90.9 MB (65.16%) - LeetHub

--- 1807-evaluate-the-bracket-pairs-of-a-string/1807-evaluate-the-bracket-pairs-of-a-string.java
+++ 1807-evaluate-the-bracket-pairs-of-a-string/1807-evaluate-the-bracket-pairs-of-a-string.java
@@ -4,26 +4,20 @@ class Solution {
         for (List<String> entry : knowledge) {
             hm.put(entry.get(0), entry.get(1));
         }
-        StringBuilder sb = new StringBuilder();
         StringBuilder ans = new StringBuilder();
-        boolean bracket = false;
-        for (char c : s.toCharArray()) {
-            if (c == '(') {
-                bracket = true;
-                continue;
+        int i=0;
+        while (i < s.length()) {
+            if (s.charAt(i) == '(') {
+                int start = ++i;
+                while (s.charAt(i) != ')') {
+                    i++;
+                }
+                String key = s.substring(start, i);
+                ans.append(hm.getOrDefault(key, "?"));
+                i++;
+            } else {
+                ans.append(s.charAt(i++));
             }
-            else if (c == ')') {
-                bracket = false;
-                String key = sb.toString();
-                if (!hm.containsKey(key)) ans.append('?');
-                else ans.append(hm.get(key));
-                sb.setLength(0);
-                continue;
-            }
-            if (bracket) {
-                sb.append(c);
-            }
-            else ans.append(c);
         }
         return ans.toString();
     }
