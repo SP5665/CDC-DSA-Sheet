bfd9c7fe7135551a0772703778482f47971f0001
Srishti Pandey <srishtipandey0512@gmail.com>
2026-09-26 19:17:42 +0530

    Time: 39 ms (46.15%), Space: 90.1 MB (92.31%) - LeetHub

--- 1807-evaluate-the-bracket-pairs-of-a-string/1807-evaluate-the-bracket-pairs-of-a-string.java
+++ 1807-evaluate-the-bracket-pairs-of-a-string/1807-evaluate-the-bracket-pairs-of-a-string.java
@@ -1,8 +1,8 @@
 class Solution {
     public String evaluate(String s, List<List<String>> knowledge) {
         Map<String, String> hm = new HashMap<>();
-        for (int i=0; i<knowledge.size(); i++) {
-            hm.put(knowledge.get(i).get(0), knowledge.get(i).get(1));
+        for (List<String> entry : knowledge) {
+            hm.put(entry.get(0), entry.get(1));
         }
         StringBuilder sb = new StringBuilder();
         StringBuilder ans = new StringBuilder();
